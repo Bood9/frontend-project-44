8e0c1f8e9c51231b9c04bf9824b349b893bb28a6
Bood9 <gallerb@bk.ru>
2024-03-23 16:52:50 +0300

    step 5 corect

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,35 +1,16 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greeting from "../src/cli.js";
-
-const userName = greeting();
-
+import { playGame, getRandomNumber } from '../src/Nikola.js';
  
- console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const task = () => {
-    
-    let a = '';
-    if (q % 2 === 0) {
-        a='yes';
-    } else {
-        a='no';
-    }
-    return [q,a]
+const isEven = (number) => number % 2 === 0;
+const generateRound = () => {
+    const question = getRandomNumber(1, 100);
+    const correctAnswer = isEven(question) ? 'yes' : 'no';
+    return[question, correctAnswer];
 };
 
-for (let i = 0; i < 3; i += 1) {
-    const step = task();
-    console.log(`Question: ${step[0]}`);
-    const answer = readlineSync.question('Answer: ');
-    
-    if (answer === step[1]) {
-        console.log('Correct!');
-    } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${step[1]}'.`);
-        console.log(`Let's try again, ${userName}!`);
-        break;
-    }
-}
-
-
+const startBrainEven = () => {
+    playGame(rules, generateRound);
+};
+startBrainEven();
---
+++ src/Nikola.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+const round = 3;
+export const playGame = (rules, generateRound) => {
+     
+        console.log('Welcome to the Brain Games!');
+        const name = readlineSync.question('May I have your name? ');
+        console.log(`Hello, ${name}!`);
+        console.log(rules); 
+        for (let i = 0; i < round; i += 1) {
+            const [question, correctAnswer] = generateRound();
+            console.log(`Question: ${question}`);
+            const answer = readlineSync.question('Answer: ');
+            
+            if (answer === correctAnswer) {
+                console.log('Correct!');
+            } else {
+                console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+                console.log(`Let's try again, ${name}!`);
+                break;
+            }
+        }
+        
+      
+   
+   
+    };
+    
+    export const getRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min);
+
+
+13
