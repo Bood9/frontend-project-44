362b87725a31f3d2ca1540ec2803b7419b91ebdc
Bood9 <gallerb@bk.ru>
2024-03-23 15:10:30 +0300

    step 5

---
+++ bin/brain-even.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import greeting from "../src/cli.js";
+
+const userName = greeting();
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const task = () => {
+    const q = Math.round(Math.random() * 100);
+    let a = '';
+    if (q % 2 === 0) {
+        a='yes';
+    } else {
+        a='no';
+    }
+    return [q,a]
+};
+
+for (let i = 0; i < 3; i += 1) {
+    const step = task();
+    console.log(`Question: ${step[0]}`);
+    const answer = readlineSync.question('Answer: ');
+    
+    if (answer === step[1]) {
+        console.log('Correct!');
+    } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${step[1]}'.`);
+        console.log(`Let's try again, ${userName}!`);
+        break;
+    }
+}
+
+
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,11 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+const greeting = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
 
-export const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
+    return name;
+};
+
+export default greeting;
