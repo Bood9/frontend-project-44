8b52e38aba979d33469c65890c45edbd845d1356
Bood9 <gallerb@bk.ru>
2024-04-03 14:13:05 +0300

    finaliti test

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,12 +5,12 @@ const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (number) => number % 2 === 0;
 const generateRound = () => {
-    const question = getRandomNumber(1, 100);
-    const correctAnswer = isEven(question) ? 'yes' : 'no';
-    return [question, correctAnswer];
+  const question = getRandomNumber(1, 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
 };
 
 const startBrainEven = () => {
-    playGame(rules, generateRound);
+  playGame(rules, generateRound);
 };
 startBrainEven();
--- src/Nikola.js
+++ src/Nikola.js
@@ -2,28 +2,28 @@ import readlineSync from 'readline-sync';
 
 const round = 3;
 export const playGame = (rules, generateRound) => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-    console.log(rules);
-    for (let i = 0; i < round; i += 1) {
-        const [question, correctAnswer] = generateRound();
-        console.log(`Question: ${question}`);
-        const answer = readlineSync.question('Answer: ');
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(rules);
+  for (let i = 0; i < round; i += 1) {
+    const [question, correctAnswer] = generateRound();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Answer: ');
 
-        if (answer === correctAnswer) {
-            console.log('Correct!');
-        } else {
-            console.log(
-                `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-            );
-            console.log(`Let's try again, ${name}!`);
-            break;
-        }
-        if (i === round - 1) {
-            console.log(`Congratulations, ${name}!`);
-        }
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(
+        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+      );
+      console.log(`Let's try again, ${name}!`);
+      break;
     }
+    if (i === round - 1) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
 };
 
 const newLocal = (min, max) => Math.round(Math.random() * (max - min) + min);
