c7d49ed824e277a5455c5862275310bc159ac4f4
Bood9 <gallerb@bk.ru>
2024-04-03 11:59:22 +0300

    testind 9 s

--- src/Nikola.js
+++ src/Nikola.js
@@ -1,34 +1,34 @@
 import readlineSync from 'readline-sync';
 const round = 3;
 export const playGame = (rules, generateRound) => {
-     
-        console.log('Welcome to the Brain Games!');
-        const name = readlineSync.question('May I have your name? ');
-        console.log(`Hello, ${name}!`);
-        console.log(rules); 
-        for (let i = 0; i < round; i += 1) {
-            const [question, correctAnswer] = generateRound();
-            console.log(`Question: ${question}`);
-            const answer = readlineSync.question('Answer: ');
-            
-            if (answer === correctAnswer) {
-                console.log('Correct!');
-            } else {
-                console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-                console.log(`Let's try again, ${name}!`);
-                break;
-                
-            }
-            if (i === round - 1) {
-                console.log(`Congratulations, ${name}!`);
-            }
+
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    console.log(rules);
+    for (let i = 0; i < round; i += 1) {
+        const [question, correctAnswer] = generateRound();
+        console.log(`Question: ${question}`);
+        const answer = readlineSync.question('Answer: ');
+
+        if (answer === correctAnswer) {
+            console.log('Correct!');
+        } else {
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+            console.log(`Let's try again, ${name}!`);
+            break;
+
         }
-        
-      
-   
-   
-    };
-    
-    export const getRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min);
+        if (i === round - 1) {
+            console.log(`Congratulations, ${name}!`);
+        }
+    }
+
+
+
+
+};
+
+export const getRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min);
 
 
